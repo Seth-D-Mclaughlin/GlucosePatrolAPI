77e8bedc87d651fd183314726d1f3d0f9d69c882
Greg Murray <gremurra@gmail.com>
2020-04-30 17:50:07 -0400

    added EventController, Get and Post methods

---
+++ GlucosePatrol.WebAPI/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using GlucosePatrol.Models;
+using GlucosePatrol.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GlucosePatrol.WebAPI.Controllers
+{
+    [Authorize]
+    public class EventController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(EventListItem model)
+        {
+            var eventService = new EventService(model.PatientId);
+            var events = eventService.GetEvents();
+            return Ok(events);
+        }
+        [HttpPost]
+        public IHttpActionResult Post(EventCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = new EventService(model.PatientId);
+
+            if (!service.CreateEvent(model))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+
+}
