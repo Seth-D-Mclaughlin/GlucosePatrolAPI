c06b447e921f1c479b8646350175cecf02f89ccc
Greg Murray <gremurra@gmail.com>
2020-05-01 11:06:19 -0400

    added comments to DataLayer

--- GlucosePatrol.Data/Entry.cs
+++ GlucosePatrol.Data/Entry.cs
@@ -11,17 +11,17 @@ namespace GlucosePatrol.Data
     public class Entry
     {
         [Key]
-        public int EntryId { get; set; } // Gives the ability to identify specfic readings
+        public int EntryId { get; set; } // Gives the ability to identify specific readings
 
         [Required]
-        [Range(30, 700)] //The reading        **Limit the reading to as low as 30 and high as 700**
+        [Range(30, 700)] //The reading  **Limit the reading to as low as 30 and high as 700**
         public int BloodSugarReading { get; set; } 
         [Required]
         public DateTimeOffset CreatedUtc { get; set; } //When the reading was created
         public DateTimeOffset? ModifiedUtc { get; set; }// When/if the reading was modified
 
         public int PatientId { get; set; } //Every entry is linked to a patientId 
-        [ForeignKey(nameof(PatientId))]    //Patients class has a one to many realtionship with Entry
+        [ForeignKey(nameof(PatientId))]    //Patients class has a one-to-many relationship with Entry
         public virtual Patient Patient { get; set; } //Gives acesss to Patient properites
     }
 }
--- GlucosePatrol.Data/Event.cs
+++ GlucosePatrol.Data/Event.cs
@@ -8,31 +8,31 @@ using System.Threading.Tasks;
 
 namespace GlucosePatrol.Data
 {
-    //We allow our user (patient) to log and Event (EventType)
-    //With this project centered on diabetes our events (EventType) were focused on things that can have a direct impact on Blood Sugar levels
+    //We allow our user (patient) to log an event (EventType)
+    //With this project centered on diabetes, our events (EventType) were focused on things that can have a direct impact on blood sugar levels
     public enum EventType {carbs = 1, insulin, exercise, health }
 
-    //Below we have Sub Categories that our user (patient) can provide a more specific description for the above events (EventType)
+    //Below we have subcategories where our user (patient) can provide more specifics for their EventType
     public enum EventSubType {fastActing = 1, longActing, light, medium, heavy, illness, stress, highSymptoms, lowSymptoms, cycle, alcohol }
     
-    //Below we have a specific units the user (patient) can choose to further describe there event 
+    //Below we have specific units the user (patient) can choose to further describe their event 
     public enum UnitType { grams = 1, units, minutes}
     public class Event
     {
         [Key]
-        public int EventId { get; set; } //EventId lets us specify a certain event 
+        public int EventId { get; set; } //EventId specifies a certain event 
         [Required]
         public EventType TypeOfEvent { get; set; }
         public EventSubType? SubTypeOfEvent { get; set; }
-        public float? Value { get; set; } //If quantifiable this is where we save the specific value of said Event. We leave nullable because some Health events are not quantifiable.
+        public float? Value { get; set; } //This is where we save the specific value of said Event. Value is nullable because certain events are not quantifiable (stress, cycle, etc.)
         public UnitType? Unit { get; set; }
         [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
-        public DateTimeOffset? ModifiedUtc { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; } //When the event was created
+        public DateTimeOffset? ModifiedUtc { get; set; } //When/if the event was modified
         
         
         public int PatientId { get; set; }  //Every entry is linked to a patientId
-        [ForeignKey(nameof(PatientId))]     //Patients class has a one to many relationship with Entry
+        [ForeignKey(nameof(PatientId))]     //Patient class has a one-to-many relationship with Event
         public virtual Patient Patient { get; set; }
     }
 }
